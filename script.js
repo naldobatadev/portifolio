4ff6552ec0206b2d49f2ba71bd5ddf6441170a31
Naldobata <naldorobertobata@gmail.com>
2025-10-12 18:07:17 +0200

    v

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 var btn = document.getElementById("showhide");
 var contador = 0;
 
@@ -16,4 +18,58 @@ btn.addEventListener("click", function showHide() {
   
   
   
+});
+
+
+*/
+
+var next = document.querySelector("button#next");
+var prev = document.querySelector("button#prev");
+var contador = 0;
+var pcontainer = document.querySelectorAll("div.pcontainer");
+var pcontainerLength = Number(pcontainer.length);
+
+window.onload = carregar()
+
+function carregar(){
+  document.getElementById("contador").innerHTML = `0${
+    contador + 1
+  } / 0${pcontainerLength}`;
+}
+
+next.addEventListener("click", function () {
+  var pcontainerOn = document.querySelector("div.pcontainer.on");
+
+  pcontainerOn.classList.remove("on");
+
+  if (contador != Number(pcontainerLength + 1)) {
+    contador++;
+  }
+  if (contador == pcontainerLength) {
+    contador = 0;
+  }
+
+  pcontainer[contador].classList.add("on");
+
+  document.getElementById("contador").innerHTML = `0${
+    contador + 1
+  } / 0${pcontainerLength}`;
+});
+
+prev.addEventListener("click", function () {
+  var pcontainerOn = document.querySelector("div.pcontainer.on");
+
+  pcontainerOn.classList.remove("on");
+
+  if (contador == 0) {
+    contador = pcontainerLength - 1;
+  } else {
+    contador--;
+  }
+
+  pcontainer[contador].classList.add("on");
+
+  document.getElementById("contador").innerHTML = `0${
+    contador + 1
+  } / 0${pcontainerLength}`;
 });
