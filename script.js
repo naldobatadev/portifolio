22e7b0552f4a7b0dcf814a9cf5881d2ab472de9d
Naldobata <naldorobertobata@gmail.com>
2025-10-23 02:51:56 +0200

    v

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-var btnMenu = document.getElementById("btnmenu");
-btnMenu.addEventListener("click", function (e) {
-  var Menu = document.querySelector("div.menu")
-  Menu.classList.toggle("off");
-  e.stopPropagation()
-});
-
-///AO CLICAR FORA FECHA
-document.addEventListener("click", function (e) {
-  var Menu = document.querySelector("div.menu");
-  var btnMenu = document.getElementById("btnmenu");
-
-  if (!Menu.classList.contains("off") && !Menu.contains(e.target) && !btnMenu.contains(e.target)) {
-    Menu.classList.add("off");
-  }})
-
-////////////////////////////////////
+//////////-----CONTAINER----/////////
 var next = document.querySelector("button#next");
 var prev = document.querySelector("button#prev");
 var contador = 0;
@@ -65,118 +49,19 @@ prev.addEventListener("click", function () {
     contador + 1
   } / 0${pcontainerLength}`;
 });
-///////////////////////////
 
-var nextmobile = document.querySelector("button#nextmobile");
-var prevmobile = document.querySelector("button#prevmobile");
-var contadormobile = 0;
-var pcontainermobile = document.querySelectorAll("div.pcontainermobile");
-var pcontainermobileLength = Number(pcontainermobile.length);
 
-window.onload = carregar();
 
-function carregar() {
-  document.getElementById("contador").innerHTML = `0${
-    contadormobile + 1
-  } / 0${pcontainermobileLength}`;
-}
+/////////////////-------ANIMATIONS---////////////////////////////
 
-nextmobile.addEventListener("click", function () {
-  var pcontainermobileOn = document.querySelector("div.pcontainermobile.on");
-
-  pcontainermobileOn.classList.remove("on");
-
-  if (contadormobile != Number(pcontainermobileLength + 1)) {
-    contadormobile++;
-  }
-  if (contadormobile == pcontainermobileLength) {
-    contadormobile = 0;
-  }
-
-  pcontainermobile[contadormobile].classList.add("on");
-
-  document.getElementById("contador").innerHTML = `0${
-    contadormobile + 1
-  } / 0${pcontainermobileLength}`;
+const elements = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+    }
+  });
 });
 
-prevmobile.addEventListener("click", function () {
-  var pcontainermobileOn = document.querySelector("div.pcontainermobile.on");
-
-  pcontainermobileOn.classList.remove("on");
-
-  if (contadormobile == 0) {
-    contadormobile = pcontainermobileLength - 1;
-  } else {
-    contadormobile--;
-  }
-
-  pcontainermobile[contadormobile].classList.add("on");
-
-  document.getElementById("contador").innerHTML = `0${
-    contadormobile + 1
-  } / 0${pcontainermobileLength}`;
-});
-
-
-///////////////////////////////////////
-
-
-
-
-
-
-var nextmobile = document.querySelector("button#nextmobileum");
-var prevmobile = document.querySelector("button#prevmobileum");
-var contadormobile = 0;
-var pcontainermobile = document.querySelectorAll("div.pcontainermobile");
-var pcontainermobileLength = Number(pcontainermobile.length);
-
-window.onload = carregar();
-
-function carregar() {
-  document.getElementById("contador").innerHTML = `0${
-    contadormobile + 1
-  } / 0${pcontainermobileLength}`;
-}
-
-nextmobile.addEventListener("click", function () {
-  var pcontainermobileOn = document.querySelector("div.pcontainermobile.on");
-
-  pcontainermobileOn.classList.remove("on");
-
-  if (contadormobile != Number(pcontainermobileLength + 1)) {
-    contadormobile++;
-  }
-  if (contadormobile == pcontainermobileLength) {
-    contadormobile = 0;
-  }
-
-  pcontainermobile[contadormobile].classList.add("on");
-
-  document.getElementById("contador").innerHTML = `0${
-    contadormobile + 1
-  } / 0${pcontainermobileLength}`;
-});
-
-prevmobile.addEventListener("click", function () {
-  var pcontainermobileOn = document.querySelector("div.pcontainermobile.on");
-
-  pcontainermobileOn.classList.remove("on");
-
-  if (contadormobile == 0) {
-    contadormobile = pcontainermobileLength - 1;
-  } else {
-    contadormobile--;
-  }
-
-  pcontainermobile[contadormobile].classList.add("on");
-
-  document.getElementById("contador").innerHTML = `0${
-    contadormobile + 1
-  } / 0${pcontainermobileLength}`;
-});
-
-
-////////////////////////////////////////////////
-
+elements.forEach((element) => observer.observe(element));
